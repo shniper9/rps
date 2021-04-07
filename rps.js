7bcbed7fac2eecf50b4170e77e48b9d06df0ee0f
shniper9 <kyle_glodek@hotmail.com>
2021-04-07 13:48:31 -0600

    Edit rps.js

--- rps.js
+++ rps.js
@@ -4,16 +4,42 @@ const scissors = "Scissors";
 const choices = [rock, paper, scissors];
 let random = Math.floor(Math.random() * choices.length);
 let playerSelection = prompt("Rock, Paper, Scissors?")
+let computerSelection = computerPlay();
 function computerPlay() {
     console.log(random, choices[random]);
+    }
+    
+function playRound() {   
+    if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        alert("Rock beats scissors! You win!")
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        alert("Paper beats rock! You lose!")
+    }
+    else if (playerSeletion == "Rock" && computerSelection == "Rock") {
+        alert("Tie! Try again!")
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        alert("Paper beats rock! You win!")
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        alert("Scissors beats paper! You lose!")
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Paper") {
+        alert("Tie! Try again!")
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        alert("Rock beats scissors! You lose!")
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        alert("Scissors beats paper! You win!")
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+        alert("Tie! Try again!")
+    }
+    else {
+        alert("Try again!")
+    }
 }
-if (playerSelection == "Rock" && computerPlay == "Scissors") {
-    alert("Rock beats scissors! You win!")
-}
-else if (playerSelection == "Rock" && computerPlay == "Paper") {
-    alert("Paper beats rock! You lose!")
-}
-else if (playerSeletion == "Rock" && computerPlay == "Rock") {
-    alert("Tie! Try again!")
-}
-console.log(computerPlay());
+
+playRound();
