ca3aa3d3f14ea4b29c19ecad9dbfbacc7d36dfb5
shniper9 <kyle_glodek@hotmail.com>
2021-04-07 11:54:49 -0600

    Add index.html

--- rps.js
+++ rps.js
@@ -3,9 +3,17 @@ const paper = "Paper";
 const scissors = "Scissors";
 const choices = [rock, paper, scissors];
 let random = Math.floor(Math.random() * choices.length);
-
+let playerSelection = prompt("Rock, Paper, Scissors?")
 function computerPlay() {
     console.log(random, choices[random]);
 }
-
+if (playerSelection == "Rock" && computerPlay == "Scissors") {
+    alert("Rock beats scissors! You win!")
+}
+else if (playerSelection == "Rock" && computerPlay == "Paper") {
+    alert("Paper beats rock! You lose!")
+}
+else if (playerSeletion == "Rock" && computerPlay == "Rock") {
+    alert("Tie! Try again!")
+}
 console.log(computerPlay());
