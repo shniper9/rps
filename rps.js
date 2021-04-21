28f16519b6d704387eebd5f8d091899258454d78
shniper9 <kyle_glodek@hotmail.com>
2021-04-21 12:16:40 -0600

    Edit to keep score

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;  
 function playRound() { 
 const rock = "Rock";
 const paper = "Paper";
@@ -8,33 +10,39 @@ let playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
 let computerSelection = computerPlay();
     function computerPlay() {
     return(random, choices[random]);
-    }   
+    } 
     if (playerSelection === "rock" && computerSelection === "Scissors") {
-        alert("Rock beats scissors! You win!")
+        alert("Rock beats scissors! You win this round!")
+        playerScore++;
     }
     else if (playerSelection === "rock" && computerSelection === "Paper") {
-        alert("Paper beats rock! You lose!")
+        alert("Paper beats rock! You lose this round!")
+        computerScore++;
     }
     else if (playerSelection === "rock" && computerSelection === "Rock") {
-        alert("Tie! Try again!")
+        alert("Tie! Try again!");
     }
     else if (playerSelection === "paper" && computerSelection === "Rock") {
-        alert("Paper beats rock! You win!")
+        alert("Paper beats rock! You win this round!")
+        playerScore++;
     }
     else if (playerSelection === "paper" && computerSelection === "Scissors") {
-        alert("Scissors beats paper! You lose!")
+        alert("Scissors beats paper! You lose this round!")
+        computerScore++;
     }
     else if (playerSelection === "paper" && computerSelection === "Paper") {
         alert("Tie! Try again!")
     }
     else if (playerSelection === "scissors" && computerSelection === "Rock") {
-        alert("Rock beats scissors! You lose!")
+        alert("Rock beats scissors! You lose this round!")
+        computerScore++;
     }
     else if (playerSelection === "scissors" && computerSelection === "Paper") {
-        alert("Scissors beats paper! You win!")
+        alert("Scissors beats paper! You win this round!")
+        playerScore++;
     }
     else if (playerSelection === "scissors" && computerSelection === "Scissors") {
-        alert("Tie! Try again!")
+        alert("Tie Round!")
     }
     else {
         alert("Oops! Someting went wrong! Try again!")
@@ -46,5 +54,14 @@ function game() {
     playRound();
     playRound();
     playRound();
+    if (playerScore > computerScore) {
+        alert("You win the game with a score of "+ playerScore +"-" + computerScore + "!");
+    }
+    else if (playerScore === computerScore) {
+        alert("Tie Game! "+ playerScore +"-" + computerScore) + "Boring!";
+    }
+    else {
+        alert("You lose the game with a score of "+ playerScore +"-" + computerScore + "!");
+    }
 }
 game();
